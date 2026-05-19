c2eeb3f09d0ddc08db5f66ba54a073d2fab37ca9
David Knieradl <meowsidemusic@gmail.com>
2026-05-19 18:34:05 +0200

    Initialized project

---
+++ david-knieradl_ak2zc_seminar/david-knieradl_ak2zc_seminar.c
@@ -0,0 +1,12 @@
+﻿// david-knieradl_ak2zc_seminar.c : Defines the entry point for the application.
+//
+
+#include <stdio.h>
+#include "david-knieradl_ak2zc_seminar.h"
+
+
+int main()
+{
+	printf("Hello CMake.");
+	return 0;
+}
---
+++ david-knieradl_ak2zc_seminar/david-knieradl_ak2zc_seminar.h
@@ -0,0 +1,5 @@
+﻿// david-knieradl_ak2zc_seminar.h : Include file for standard system include files,
+// or project specific include files.
+
+
+// TODO: Reference additional headers your program requires here.
